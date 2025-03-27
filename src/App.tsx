c2054f2590af83b89d31aaa9da1da73cf48ebf2a
anand <goudanand19@gmail.com>
2025-03-27 18:00:14 +0530

    project 1st depo

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
+import InputFeild from './components/InputFeild';
+import { Todo } from './model';
+import TodoList from './components/TodoList';
+import { DragDropContext, DropResult } from '@hello-pangea/dnd';
 
-function App() {
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos,setTodos] = useState<Todo[]>([{id: 1742814640105, isDone: false, todo: "hello"}]);
+  const [CompletedTodos, setCompletedTodos] = useState<Todo[]>([])
+  const handleAdd = (e:React.FormEvent) => {
+    e.preventDefault();
+    if(todo){
+      setTodos([...todos,{id:Date.now(),todo,isDone:false}]);
+      setTodo("")
+    }
+  };
+
+  const onDragEnd = (result:DropResult) => {
+      const {source,destination} = result;
+      if(!destination) return;
+      if(destination.droppableId === source.droppableId && destination.index === source.index) return;
+      
+      let add,active = todos,
+      complete = CompletedTodos;
+
+      if(source.droppableId === "TodosList"){
+        add = active[source.index];
+        active.splice(source.index,1);
+      }
+      else{
+        add = complete[source.index];
+        complete.splice(source.index,1);
+      }
+      
+      if(destination.droppableId === "TodosList"){
+        active.splice(destination.index,0,add);
+      }
+      else{
+        complete.splice(destination.index,0,add);
+      }
+      setTodos(active);
+      setCompletedTodos(complete);
+    }
+
+  
   return (
+  <DragDropContext onDragEnd={onDragEnd}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span className='heading'>Taskify</span>
+      <InputFeild todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+      <TodoList todos={todos} setTodos={setTodos} completedTodos={CompletedTodos} setCompletedTodos={setCompletedTodos}/>
+      
     </div>
+  </DragDropContext>
   );
 }
 
